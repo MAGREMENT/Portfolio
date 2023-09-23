926de0098be76174bc335be2e4bbbc0573fbf6e9
Zachary VANVLASSELAER <z.vanvlasselaer@student.helmo.be>
2023-09-23 11:33:48 +0200

    first

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Portfolio</title>
+    <link rel="stylesheet" type="text/css" href="Css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather&display=swap" rel="stylesheet">
+</head>
+<body>
+    <?php include "Inc/logo.inc.html" ?>
+    <header class="MainHeader">
+        <nav>
+            <a href="aboutme.php" class="NavLinks">About me</a>
+            <a href="projects.php" class="NavLinks">Projects</a>
+            <a href="contact.php" class="NavLinks">Contact</a>
+        </nav>
+    </header>
+    <main>
+        <p class="Name FirstName">Zachary</p>
+        <p class="Name SecondName">Vanvlasselaer</p>
+        <p class="Who">I'm an <span class="Title"><span class="HighlightSecondary">App </span><span class="HighlightPrimary">&</span><span class="HighlightSecondary"> Web Developer</span></span><br>
+            from the region of Liège in Belgium, currently studying at <a class="Link" href="https://helmo.be">HELMO.</a></p>
+        <p class="Who">
+            I build robust and fast applications with a cool and simple UI design.<br>
+            I like to believe that my strength lies in my always improving code.
+        </p>
+    </main>
+</body>
+</html>
